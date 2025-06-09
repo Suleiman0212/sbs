4a75924a852f83c550591fdebcb2653b65a31e35
Kirimov Suleiman <kirimov.suleiman@gmai.com>
2025-06-09 12:31:41 +0500

    Init with working prototype.

---
+++ build.c
@@ -0,0 +1,30 @@
+#include "build.h"
+
+// Set C compiler and its flags
+$cc("gcc -Wall -Wextra -O2 -std=c17 -ffreestanding -fno-pie -fno-stack-protector")
+
+// Set assembler and its flags
+$as("nasm -f elf64")
+
+// Set linker and its flags
+$ld("ld -nostdlib -static -z noexecstack")
+
+// Set the output binary name
+$bin("bin/compressor")
+
+// Add C source files
+$csrc("src/file0.c", "src/file1.c")
+
+// Add Assembly source files
+$assrc("src/file2.s", "src/file3.s")
+
+// Set output directory for object files
+$outdir("out/")
+
+start
+  ccompile     // Compile C source files into .o files
+  ascompile    // Assemble .s files into .o files
+  link         // Link all object files into final binary
+  run          // Run the resulting binary
+end
+
---
+++ build.h
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+#define start int main() { char objsrc[1024 * 10] = {0}; system("clear"); printf("SULEKO BUILD SYSTEM.\nBUILDING %s.\n\n", binname);
+#define end printf("DONE.\n"); return 0; }
+
+#define $cc(path) const char *cc_path = path;
+#define $as(path) const char *as_path = path;
+#define $ld(path) const char *ld_path = path;
+
+#define $csrc(...) const char *csrc[] = { __VA_ARGS__ };
+#define $assrc(...) const char *assrc[] = { __VA_ARGS__ };
+
+#define $outdir(path) const char *outdir = path;
+#define $bin(name) const char *binname = name;
+
+#define ccompile { \
+  printf("C COMPILATION STARTED.\n"); \
+  for (int i = (int)(sizeof(csrc) / sizeof(csrc[0])) - 1; i >= 0; i--) { \
+    char cmd[256] = {0}; \
+    char objname[128] = {0}; \
+    const char *filename = strrchr(csrc[i], '/'); \
+    filename = filename ? filename + 1 : csrc[i]; \
+    strncpy(objname, filename, sizeof(objname) - 1); \
+    char *dot = strrchr(objname, '.'); \
+    if (dot) *dot = '\0'; \
+    snprintf(objname + strlen(objname), sizeof(objname) - strlen(objname), ".o"); \
+    snprintf(cmd, sizeof(cmd), "%s -c %s -o %s%s", cc_path, csrc[i], outdir, objname); \
+    printf("EXECUTING: %s\n", cmd); \
+    system(cmd); \
+    strcat(objsrc, outdir); \
+    strcat(objsrc, objname); \
+    strcat(objsrc, " "); \
+  } \
+  printf("C COMPILATION FINISHED.\n\n"); \
+}
+
+#define ascompile { \
+  printf("ASM COMPILATION STARTED.\n"); \
+  for (int i = (int)(sizeof(assrc) / sizeof(assrc[0])) - 1; i >= 0; i--) { \
+    char cmd[256] = {0}; \
+    char objname[128] = {0}; \
+    const char *filename = strrchr(assrc[i], '/'); \
+    filename = filename ? filename + 1 : assrc[i]; \
+    strncpy(objname, filename, sizeof(objname) - 1); \
+    char *dot = strrchr(objname, '.'); \
+    if (dot) *dot = '\0'; \
+    snprintf(objname + strlen(objname), sizeof(objname) - strlen(objname), ".o"); \
+    snprintf(cmd, sizeof(cmd), "%s -c %s -o %s%s", as_path, assrc[i], outdir, objname); \
+    printf("EXECUTING: %s\n", cmd); \
+    system(cmd); \
+    strcat(objsrc, outdir); \
+    strcat(objsrc, objname); \
+    strcat(objsrc, " "); \
+  } \
+  printf("ASM COMPILATION FINISHED.\n\n"); \
+}
+
+#define link { \
+  printf("LINKING STARTED.\n"); \
+  char cmd[1024 * 10] = {0}; \
+  snprintf(cmd, sizeof(cmd), "%s %s -o %s", ld_path, objsrc, binname); \
+  printf("EXECUTING: %s\n", cmd); \
+  system(cmd); \
+  printf("LINKING FINISHED.\n\n"); \
+}
+
+#define run { \
+  char cmd[256] = {0}; \
+  snprintf(cmd, sizeof(cmd), "./%s", binname); \
+  system(cmd); \
+}
